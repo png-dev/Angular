a478e4aec8d62575b747e4c264e7231c6a33e7eb
PNG <phongng@wilad.vn>
2020-04-16 17:17:44 +0700

    Add component snack-bar

--- src/app/snack-bar/snack-bar.component.ts
+++ src/app/snack-bar/snack-bar.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {SnackbarService} from '../services/snackbar.service';
+
 
 @Component({
-  selector: 'app-snack-bar',
-  templateUrl: './snack-bar.component.html',
-  styleUrls: ['./snack-bar.component.scss']
+    selector: 'app-snack-bar',
+    templateUrl: './snack-bar.component.html',
+    styleUrls: ['./snack-bar.component.scss']
 })
-export class SnackBarComponent implements OnInit {
+export class SnackBarComponent implements OnInit, OnDestroy {
+    private subscription: Subscription;
+    message: any;
 
-  constructor() { }
+    constructor(
+        private snackbarService: SnackbarService
+    ) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.subscription = this.snackbarService.getAlert()
+            .subscribe(message => {
+                switch (message && message.type) {
+                    case 'success':
+                        message.cssClass = 'alert alert-success';
+                        break;
+                    case 'error':
+                        message.cssClass = 'alert alert-danger';
+                        break;
+                }
+
+                this.message = message;
+            });
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 
 }
